cbbf06df82eaa383f9819cb51bc4a7e60bf7a9fc
Calderon Algalobos Roberto David <rdcalderon@unitru.edu.pe>
2025-07-19 20:47:29 -0500

    SE MOVIO EL METODO LISTAR POR GARAJE, A GARAJE

--- src/datos/DALGaraje.java
+++ src/datos/DALGaraje.java
@@ -4,6 +4,7 @@
  */
 package datos;
 import entidades.Garaje;
+import entidades.Automovil;
 import java.sql.*;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
@@ -137,6 +138,42 @@ public class DALGaraje {
                 cn.close();
             } catch (SQLException ex) {
                 JOptionPane.showMessageDialog(null, ex.getMessage(), "Error", 0);
+            }
+        }
+        return lista;
+    }
+    
+     public static ArrayList<Automovil> listarAutomovilesPorGaraje(String garaje) {
+        ArrayList<Automovil> lista = new ArrayList<>();
+        try {
+            cn = Conexion.realizarconexion();
+            cs = cn.prepareCall("{call sp_listar_automovil_por_garaje(?)}");
+            cs.setString(1, garaje);
+            rs = cs.executeQuery();
+            while (rs.next()) {
+                lista.add(new Automovil(
+                        rs.getString("placa"),
+                        rs.getString("modelo"),
+                        rs.getString("color"),
+                        rs.getString("marca"),
+                        rs.getString("garaje")
+                ));
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Error", 0);
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (cs != null) {
+                    cs.close();
+                }
+                if (cn != null) {
+                    cn.close();
+                }
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, e.getMessage(), "Error", 0);
             }
         }
         return lista;
