992bbd824e83390ce4c55cdd5e2cc08e51438fbf
Calderon Algalobos Roberto David <rdcalderon@unitru.edu.pe>
2025-07-19 20:32:00 -0500

    ADICION METODO LISTAR POR GARAJE

--- src/datos/DALAutomovil.java
+++ src/datos/DALAutomovil.java
@@ -3,15 +3,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package datos;
+
 import entidades.Automovil;
 import java.sql.*;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
+
 /**
  *
  * @author Asus
  */
 public class DALAutomovil {
+
     private static Connection cn;
     private static CallableStatement cs;
     private static ResultSet rs;
@@ -39,8 +42,8 @@ public class DALAutomovil {
         }
         return mensaje;
     }
-    
-        public static Automovil buscarAutomovilPorPlaca(String placaAuto) {
+
+    public static Automovil buscarAutomovilPorPlaca(String placaAuto) {
         Automovil auto = null;
         try {
             cn = Conexion.realizarconexion();
@@ -78,18 +81,18 @@ public class DALAutomovil {
         }
         return auto;
     }
-        
-        public static String actualizarAutomovil(Automovil auto) {
+
+    public static String actualizarAutomovil(Automovil auto) {
         String mensaje = null;
         try {
             cn = Conexion.realizarconexion();
             String sql = "{call sp_actualizar_automovil(?, ?, ?, ?, ?)}";
             cs = cn.prepareCall(sql);
-            cs.setString(1,auto.getPlaca());
-            cs.setString(2,auto.getModelo());
-            cs.setString(3,auto.getColor());
-            cs.setString(4,auto.getMarca());
-            cs.setInt(5,auto.getGarajeId());
+            cs.setString(1, auto.getPlaca());
+            cs.setString(2, auto.getModelo());
+            cs.setString(3, auto.getColor());
+            cs.setString(4, auto.getMarca());
+            cs.setInt(5, auto.getGarajeId());
             cs.executeUpdate();
         } catch (ClassNotFoundException | SQLException ex) {
             mensaje = ex.getMessage();
@@ -107,14 +110,14 @@ public class DALAutomovil {
         }
         return mensaje;
     }
-        
-        public static String eliminarAutomovil(String placaAuto) {
+
+    public static String eliminarAutomovil(String placaAuto) {
         String mensaje = null;
         try {
             cn = Conexion.realizarconexion();
             String sql = "{call sp_eliminar_automovil(?)}";
             cs = cn.prepareCall(sql);
-            cs.setString(1,placaAuto);
+            cs.setString(1, placaAuto);
             cs.executeUpdate();
         } catch (ClassNotFoundException | SQLException ex) {
             mensaje = ex.getMessage();
@@ -132,7 +135,7 @@ public class DALAutomovil {
         }
         return mensaje;
     }
-        
+
     public static String verificarDisponibilidadAutomovil(String placa) {
         String estado = null;
         try {
@@ -149,16 +152,21 @@ public class DALAutomovil {
             ex.printStackTrace();
         } finally {
             try {
-                if (rs != null) rs.close();
-                if (cs != null) cs.close();
-                if (cn != null) cn.close();
+                if (rs != null) {
+                    rs.close();
+                }
+                if (cs != null) {
+                    cs.close();
+                }
+                if (cn != null) {
+                    cn.close();
+                }
             } catch (SQLException ex) {
                 ex.printStackTrace();
             }
         }
         return estado;
     }
-
 
     public static ArrayList<Automovil> listarAutomoviles() {
         ArrayList<Automovil> lista = new ArrayList<>();
@@ -183,4 +191,41 @@ public class DALAutomovil {
         }
         return lista;
     }
+
+    public static ArrayList<Automovil> listarAutomovilesPorGaraje(String garaje) {
+        ArrayList<Automovil> lista = new ArrayList<>();
+        try {
+            cn = Conexion.realizarconexion();
+            cs = cn.prepareCall("{call sp_listar_automovil_por_garaje(?)}");
+            cs.setString(1, garaje);
+            rs = cs.executeQuery();
+            while (rs.next()) {
+                lista.add(new Automovil(
+                        rs.getString("placa"),
+                        rs.getString("modelo"),
+                        rs.getString("color"),
+                        rs.getString("marca"),
+                        rs.getString("garaje")
+                ));
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Error", 0);
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (cs != null) {
+                    cs.close();
+                }
+                if (cn != null) {
+                    cn.close();
+                }
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, e.getMessage(), "Error", 0);
+            }
+        }
+        return lista;
+    }
+
 }
