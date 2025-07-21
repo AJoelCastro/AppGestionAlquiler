80ef95694d57704136fff357b2e67611c31d51fc
Calderon Algalobos Roberto David <rdcalderon@unitru.edu.pe>
2025-07-20 20:14:40 -0500

    INFIMO CAMBIO

--- src/logica/BLReserva.java
+++ src/logica/BLReserva.java
@@ -15,72 +15,70 @@ import static javax.swing.JOptionPane.showMessageDialog;
  * @author Asus
  */
 public class BLReserva {
-    public static int insertarReserva(int reservaId, int IdCliente, int IdAgencia, GregorianCalendar fechaI, GregorianCalendar fechaF, double precioT, boolean estado) {
-        if (IdCliente > 0 && IdCliente > 0 && IdAgencia > 0
-                && fechaI != null && fechaF != null
-                && precioT >= 0) {
 
+    public static int insertarReserva(int reservaId, int IdCliente, int IdAgencia, GregorianCalendar fechaI, GregorianCalendar fechaF, double precioT, boolean estado) {
+        if (IdCliente > 0 && IdAgencia > 0 && fechaI != null && fechaF != null && precioT >= 0) {
             Reserva reserva = new Reserva(reservaId, IdCliente, IdAgencia, fechaI, fechaF, precioT, estado);
             int mensaje = DALReserva.insertarReserva(reserva);
 
-            if (mensaje == -1) {
+            if (mensaje > 0) {
                 showMessageDialog(null, "Reserva registrada", "Resultado", 1);
-                return 0;
+                return 0; // todo bien
             } else {
-                showMessageDialog(null, mensaje, "Error", 0);
+                showMessageDialog(null, "Error al insertar reserva", "Error", 0);
                 return 1;
             }
-
         } else {
             showMessageDialog(null, "Datos no válidos", "Error", 0);
             return 3;
         }
     }
-    
-    public static Reserva buscarReservaPorId(int idReserva){
-       ArrayList<Reserva> listaR = DALReserva.listarReservas();
-       for(Reserva reserva: listaR){
-           if(reserva.getReservaId()==idReserva);
-               return DALReserva.buscarReservaPorId(idReserva);
-       }
-       return null;
+
+    public static Reserva buscarReservaPorId(int idReserva) {
+        ArrayList<Reserva> listaR = DALReserva.listarReservas();
+        for (Reserva reserva : listaR) {
+            if (reserva.getReservaId() == idReserva);
+            return DALReserva.buscarReservaPorId(idReserva);
+        }
+        return null;
     }
-    
-    public static Reserva buscarReservaPorIdCliente(int idCliente){
-       ArrayList<Reserva> listaR = DALReserva.listarReservas();
-       for(Reserva reserva: listaR){
-           if(reserva.getClienteId()==idCliente);
-               return DALReserva.buscarReservaPorIdCliente(idCliente);
-       }
-       return null;
+
+    public static Reserva buscarReservaPorIdCliente(int idCliente) {
+        ArrayList<Reserva> listaR = DALReserva.listarReservas();
+        for (Reserva reserva : listaR) {
+            if (reserva.getClienteId() == idCliente)
+                return DALReserva.buscarReservaPorIdCliente(idCliente);
+        }
+        return null;
     }
-    
+
     public static boolean editarReserva(int reservaId, double nuevoPrecioT, boolean cambiarEstado) {
         Reserva reserva = buscarReservaPorId(reservaId);
-            if (reserva != null) {
-                reserva.setPrecioTotal(nuevoPrecioT);
-                reserva.setEntregado(cambiarEstado);
-                DALReserva.actualizarReserva(reserva);
-                return true; 
-            }
-    return false; 
+        if (reserva != null) {
+            reserva.setPrecioTotal(nuevoPrecioT);
+            reserva.setEntregado(cambiarEstado);
+            DALReserva.actualizarReserva(reserva);
+            return true;
+        }
+        return false;
     }
-    
+
     public static boolean eliminarReserva(int idReserva) {
         Reserva reserva = buscarReservaPorId(idReserva);
-            if (reserva != null) {
-                if(reserva.isEntregado()){
-                    DALReserva.eliminarReserva(idReserva);
-                    return true; }
+        if (reserva != null) {
+            if (reserva.isEntregado()) {
+                DALReserva.eliminarReserva(idReserva);
+                return true;
             }
-    return false; 
-}
+        }
+        return false;
+    }
 
     public static ArrayList<Reserva> listarReservas() {
         return DALReserva.listarReservas();
     }
-    
+
     public static ArrayList<Reserva> listarReservasActivas() {
         return DALReserva.listarReservasActivas();
-}
+    }
 }
