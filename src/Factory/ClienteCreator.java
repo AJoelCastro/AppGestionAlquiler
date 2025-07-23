e7ac1eac18b2f7115122b2661ee3907ce43480d1
Calderon Algalobos Roberto David <rdcalderon@unitru.edu.pe>
2025-07-22 22:06:25 -0500

    FACTORY

---
+++ src/Factory/ClienteCreator.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package factory;
+
+import entidades.Cliente;
+
+public abstract class ClienteCreator {
+
+    public abstract Cliente crearCliente();
+}
